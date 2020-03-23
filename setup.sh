bad0b7bebce89109f891e6a12e48973b4900e695
Multezem Kedir <multekedir@users.noreply.github.com>
2020-03-23 11:10:12 -0700

    added setup manger
    Signed-off-by: Multezem Kedir <multekedir@users.noreply.github.com>

--- setup.sh
+++ setup.sh
@@ -6,7 +6,11 @@ sudo apt-get install -y python3 python3-pip python3-virtualenv nginx gunicorn
 sudo mkdir /home/www && cd /home/www
 
 git clone https://github.com/multekedir/Prayer-Time-Display-2.git && cd Prayer-Time-Display-2
-Python3 -m pip install -r requirement.txt
+python3 -m pip install pipenv
+python3 -m pipenv shell
+pipenv install
+
+
 sudo /etc/init.d/nginx start
 sudo rm /etc/nginx/sites-enabled/default
 sudo touch /etc/nginx/sites-available/Prayer-Time-Display-2
@@ -19,12 +23,12 @@ sudo echo "server {
         proxy_set_header X-Real-IP $remote_addr;
     }
     location /static {
-        alias  /home/www/flask_project/static/;
+        alias  /home/www/Prayer-Time-Display-2/static/;
     }
 }"  > /etc/nginx/sites-enabled/Prayer-Time-Display-2
 
 sudo /etc/init.d/nginx restar
 
-cd /home/www/flask_project/
+cd /home/www/Prayer-Time-Display-2/
 gunicorn app:app -b localhost:5000
 
