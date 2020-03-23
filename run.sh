265a93f889b44f594c882ecdd46ad98b760fe519
Multezem Kedir <multekedir@users.noreply.github.com>
2020-03-23 10:01:12 -0700

    Update run.sh
    fixed python version

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 clear
 export SETTINGS=./dev.settings
-python3.7 -m flask run
+python3 -m flask run
 firefox -url http://127.0.0.1:5000/ & xdotool search --sync --onlyvisible --class "Firefox" windowactivate key F11
